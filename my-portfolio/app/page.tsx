0569198476905b8f253fd6d8bb9d49420017a54c
DeporS <depores1337@gmail.com>
2025-03-11 01:06:40 +0100

    Change font size on mobile

--- my-portfolio/app/page.tsx
+++ my-portfolio/app/page.tsx
@@ -4,10 +4,12 @@ import About from "@/components/About";
 export default function Home() {
   return (
     <main className="bg-gray-900 text-white">
-      <section className="h-screen flex flex-col justify-center items-center text-center">
-        <h1 className="text-5xl font-bold">Hi, I'm Krzysztof Matyla 👋</h1>
-        <p className="text-xl mt-4">
-          Python Developer | Data Engineer | Android Dev
+      <section className="h-screen flex flex-col justify-center items-center text-center px-4">
+        <h1 className="text-4xl sm:text-5xl font-bold">
+          Hi, I'm Krzysztof Matyla 👋
+        </h1>
+        <p className="text-lg sm:text-xl mt-4">
+          Python & Android Developer | Data Engineer
         </p>
         <div className="mt-6 flex gap-4">
           <a className="px-6 py-2 bg-blue-500 rounded-lg">CV</a>
